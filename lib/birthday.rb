938730a73a1405a7bc14ddd41ee2eac2874ffdf7
Douglas <douglaskerins@gmail.com>
2021-09-22 13:17:39 +0100

    Add name to output of birthday list

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,18 +1,20 @@
 class Birthday
+  attr_reader :name
   attr_reader :month
   attr_reader :day
   attr_reader :year
   attr_reader :date
 
-  def initialize(month, day, year)
+  def initialize(name, month, day, year)
+    @name = name
     @month = month
     @day = day
     @year = year
-    @date = join_date(@month, @day, @year)
+    @date = name_date(@name, @month, @day, @year)
   end
 
-  def join_date(month, day, year)
-    "#{month} #{day}, #{year}"
+  def name_date(name, month, day, year)
+    "#{name}: #{month} #{day}, #{year}"
   end
 
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -2,16 +2,16 @@ require 'birthday_list'
 require "birthday"
 
 describe BirthdayList do
-  it "returns a list of birthdays" do
+  it "returns a list of names and birthdays" do
     #Arrange
     birthday_list = BirthdayList.new
-    birthday = Birthday.new("April", 30, 1990)
-    expected_output = "April 30, 1990"
+    birthday = Birthday.new("Doug", "April", 30, 1990)
+    expected_output = "Doug: April 30, 1990"
     
     #Act
     birthday_list.add(birthday.date)
 
     #Assert
     expect(birthday_list.print_list).to(eq(expected_output))
-  end 
+  end
 end
