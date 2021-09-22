72204f958e7adcd416a02de53b0aba85a06288bb
Douglas <douglaskerins@gmail.com>
2021-09-22 13:46:10 +0100

    Adds second list item to test

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,11 +5,13 @@ describe BirthdayList do
   it "returns a list of names and birthdays" do
     #Arrange
     birthday_list = BirthdayList.new
-    birthday = Birthday.new("Doug", "April", 30, 1990)
-    expected_output = "Doug: April 30, 1990"
+    birthday1 = Birthday.new("Doug", "April", 30, 1990)
+    birthday2 = Birthday.new("Pat", "November", 28, 1990)
+    expected_output = "Doug: April 30, 1990, Pat: November 28, 1990"
     
     #Act
-    birthday_list.add(birthday.date)
+    birthday_list.add(birthday1.date)
+    birthday_list.add(birthday2.date)
 
     #Assert
     expect(birthday_list.print_list).to(eq(expected_output))
